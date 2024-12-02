417448c69c907631b800875e19f97079e9f18fdc
Grace Juan <grace.wj@berkeley.edu>
2024-12-01 16:44:46 -0800

    texture loop

--- main.js
+++ main.js
@@ -1,15 +1,36 @@
+/* background noise effect */
+const lightBackgrounds = ["/images/noise-light-1.jpg", "/images/noise-light-2.jpg", "/images/noise-light-3.jpg", "/images/noise-light-4.jpg"];
+const darkBackgrounds = ["/images/noise-dark-1.jpg", "/images/noise-dark-2.jpg", "/images/noise-dark-3.jpg", "/images/noise-dark-4.jpg"];
+let backgroundImgs = lightBackgrounds;
+let loopIndex = 0;
+const background = document.getElementById('background-img');
+
+const wakeSunImgs = ["/images/noise-wake-sun-1.png", "/images/noise-wake-sun-2.png", "/images/noise-wake-sun-3.png"];
+const sleepSunImgs = ["/images/noise-sleep-sun-1.png", "/images/noise-sleep-sun-2.png", "/images/noise-sleep-sun-3.png"];
+let sunImgs = wakeSunImgs;
+const sun = document.getElementById('sun');
+
+function loopBackground() {
+    loopIndex = (loopIndex + 1) % backgroundImgs.length;
+    background.src = backgroundImgs[loopIndex];
+}
+function loopSun() {
+    loopIndex = (loopIndex + 1) % sunImgs.length;
+    sun.src = sunImgs[loopIndex];
+}
+
+let backgroundInterval = setInterval(loopBackground, 150);
+setInterval(loopSun, 150);
+
 /* sleep-wake switching */
 
 const txtBtn = document.getElementById('txt-btn');
-const sun = document.getElementById('sun');
 const eyeHighlights = document.getElementById('eye-highlights');
 const factTexts = document.querySelectorAll('.fact-text');
 const redSleep = "/images/red-sleep.png";
 const whiteSleep = "/images/white-sleep.png";
 const redWake = "/images/red-wake.png";
 const whiteWake = "/images/white-wake.png";
-const sleepSun = "/images/sleep-sun.png";
-const wakeSun = "/images/wake-sun.png";
 sleep = false;
 
 txtBtn.addEventListener('mouseenter', () => {
@@ -30,7 +51,8 @@ txtBtn.addEventListener('mouseleave', () => {
 
 txtBtn.addEventListener('click', () => { 
     if (sleep) {
-        sun.src = wakeSun;
+        sun.src = "/images/noise-wake-sun-1.png";
+        sunImgs = wakeSunImgs;
         txtBtn.src = whiteSleep;
         txtBtn.classList.remove('wake');
         txtBtn.classList.add('sleep');
@@ -38,10 +60,20 @@ txtBtn.addEventListener('click', () => {
         factTexts.forEach(fact => {
             fact.classList.remove('show');
         })
-        document.body.style.backgroundColor = '#ba9caf';
+        clearInterval(backgroundInterval);
+        background.style.opacity = 0;
+        document.body.style.backgroundColor = '#826f7b';
+        setTimeout(() => {
+            backgroundImgs = lightBackgrounds;
+            background.src = "/images/noise-light-1.jpg";
+            backgroundInterval = setInterval(loopBackground, 150);
+            background.style.opacity = 1;
+        }, 300);
+
         sleep = false;
     } else {
-        sun.src = sleepSun;
+        sun.src = "/images/noise-sleep-sun-1.png";
+        sunImgs = sleepSunImgs;
         txtBtn.src = whiteWake;
         txtBtn.classList.remove('sleep');
         txtBtn.classList.add('wake');
@@ -49,7 +81,16 @@ txtBtn.addEventListener('click', () => {
         factTexts.forEach(fact => {
             fact.classList.add('show');
         })
+        clearInterval(backgroundInterval);
+        background.style.opacity = 0;
         document.body.style.backgroundColor = '#251f23';
+        setTimeout(() => {
+            backgroundImgs = darkBackgrounds;
+            background.src = "/images/noise-dark-1.jpg";
+            backgroundInterval = setInterval(loopBackground, 150);
+            background.style.opacity = 1;
+        }, 300);
+
         sleep = true;
     }
 });
